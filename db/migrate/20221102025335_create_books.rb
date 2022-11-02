59c480a891a6efdfbe3f1b2782bf0df490222bf4
samorey11 <samorey@outlook.com>
2022-11-01 23:20:26 -0400

    Controller

---
+++ db/migrate/20221102025335_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.string :publishing_year
+      t.integer :num_of_pages
+      
+      t.timestamps
+    end
+  end
+end
